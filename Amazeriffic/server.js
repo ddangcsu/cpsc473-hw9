da5247fc9181abc1ad2ca7efe4f25dc7f2a5e631
David Dang <ddang.sw@gmail.com>
2016-04-29 00:19:21 -0700

    Modified body parser and correct tab from file

--- Amazeriffic/server.js
+++ Amazeriffic/server.js
@@ -2,10 +2,12 @@ var express = require("express"),
     http = require("http"),
     // import the mongoose library
     mongoose = require("mongoose"),
-    app = express();
+    app = express(),
+    parser = require("body-parser");
 
 app.use(express.static(__dirname + "/client"));
-app.use(express.bodyParser());
+app.use(parser.urlencoded({extended: true}));
+
 
 // connect to the amazeriffic data store in mongo
 mongoose.connect('mongodb://localhost/amazeriffic');
@@ -21,8 +23,9 @@ var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
 
 app.get("/todos.json", function (req, res) {
-    ToDo.find({}, function (err, toDos) {
-	res.json(toDos);
+    var exclude = {_id: 0, __v: 0};
+    ToDo.find({}).select(exclude).exec(function (err, toDos) {
+        res.json(toDos);
     });
 });
 
@@ -30,21 +33,20 @@ app.post("/todos", function (req, res) {
     console.log(req.body);
     var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
     newToDo.save(function (err, result) {
-	if (err !== null) {
-	    // the element did not get saved!
-	    console.log(err);
-	    res.send("ERROR");
-	} else {
-	    // our client expects *all* of the todo items to be returned, so we'll do
-	    // an additional request to maintain compatibility
-	    ToDo.find({}, function (err, result) {
-		if (err !== null) {
-		    // the element did not get saved!
-		    res.send("ERROR");
-		}
-		res.json(result);
-	    });
-	}
+        if (err !== null) {
+            // the element did not get saved!
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            // our client expects *all* of the todo items to be returned, so we'll do
+            // an additional request to maintain compatibility
+            ToDo.find({}, function (err, result) {
+                if (err !== null) {
+                    // the element did not get saved!
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
     });
 });
-
